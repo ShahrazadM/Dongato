82aa32fbe8ac6b39a9ca7d5684c8077033ab45ed
ShahrazadM <shahrazadlksmi@gmail.com>
2024-04-27 19:55:02 -0400

    Merge branch 'master' of https://github.com/ShahrazadM/Dongatoysupandilla.git

--- hola/src/main/java/DobgatoysupandillaServelt/kucho.java
+++ hola/src/main/java/DobgatoysupandillaServelt/kucho.java
@@ -1,7 +1,6 @@
-package prevencion.servlet;
+package DobgatoysupandillaServelt;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Inicio
+ * Servlet implementation class kucho
  */
-@WebServlet("/")
-public class Inicio extends HttpServlet {
+@WebServlet("/kucho")
+public class kucho extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Inicio() {
+    public kucho() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,13 +25,10 @@ public class Inicio extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        // Redirige a la página JSP correspondiente
-    	getServletContext().getRequestDispatcher("/Views/home.jsp").forward(request, response);
-    }
-
-  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
